da1b7a2fb770f326a5a391f5bfe0b4561ca8e616
Starkov Sergey <starkovsr@gmail.com>
2022-07-17 15:59:24 +0300

    feat: finish 8 hw

--- src/p2-homeworks/h8/HW8.tsx
+++ src/p2-homeworks/h8/HW8.tsx
@@ -1,48 +1,49 @@
-import React, {useState} from 'react'
-import {homeWorkReducer} from './bll/homeWorkReducer'
-import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import React, { useState } from 'react';
+import { homeWorkReducer } from './bll/homeWorkReducer';
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
 
-// export type UserType =
-
-const initialPeople = [
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
-]
-
-function HW8() {
-    const [people, setPeople] = useState<any>(initialPeople) // need to fix any
-
-    // need to fix any
-    const finalPeople = people.map((p: any) => (
-        <div key={p._id}>
-            some name, age
-        </div>
-    ))
-
-    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'}))
-
-    return (
-        <div>
-            <hr/>
-            homeworks 8
-
-            {/*should work (должно работать)*/}
-            {finalPeople}
-
-            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
-            <div>sort down</div>
-            check 18
-
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativePeople/>*/}
-            <hr/>
-        </div>
-    )
+export type UserType = {
+  _id: number
+  name: string
+  age: number
 }
 
-export default HW8
+const initialPeople: UserType[] = [
+  { _id: 0, name: 'Кот', age: 3 },
+  { _id: 1, name: 'Александр', age: 66 },
+  { _id: 2, name: 'Коля', age: 16 },
+  { _id: 3, name: 'Виктор', age: 44 },
+  { _id: 4, name: 'Дмитрий', age: 40 },
+  { _id: 5, name: 'Ирина', age: 55 },
+];
+
+function HW8() {
+  const [people, setPeople] = useState<UserType[]>(initialPeople); // need to fix any
+
+  // need to fix any
+  const finalPeople = people.map((p: UserType) => <div key={p._id} style={{display: 'flex', justifyContent: 'space-between', maxWidth: '250px'}}>
+    <span>{p.name}</span>
+    <span>{p.age}</span>
+  </div>);
+
+  const sortUp = () => setPeople(homeWorkReducer(people, { type: 'sort', payload: 'up' }));
+
+  const sortDown = () => setPeople(homeWorkReducer(people, { type: 'sort', payload: 'down' }));
+
+  const checkAge = () => setPeople(homeWorkReducer(people, { type: 'check', payload: 18}))
+
+  return (
+    <div>
+      <h3 className="homework-title">homeworks 8</h3>
+      {finalPeople}
+      <div style={{padding: '10px 0'}}>
+        <SuperButton onClick={sortUp}>sort up</SuperButton>
+        <SuperButton onClick={sortDown}>sort down</SuperButton>
+        <SuperButton onClick={checkAge}>check 18</SuperButton>
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default HW8;
--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,27 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
+import { UserType } from '../HW8';
+
+export type SortActionType = {
+  type: 'sort';
+  payload: 'up' | 'down';
+};
+
+export type CheckActionType = {
+  type: 'check';
+  payload: number;
+};
+
+type ActionsType = SortActionType | CheckActionType;
+
+export const homeWorkReducer = (state: UserType[], action: ActionsType): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      return [...state].sort((prev, next) => (action.payload === 'up' ? next.age - prev.age : prev.age - next.age));
     }
-}
+    case 'check': {
+      return state.filter((user) => user.age >= action.payload);
+    }
+    default:
+      return state;
+  }
+};
